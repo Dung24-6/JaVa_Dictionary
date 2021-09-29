5ba288fd6441b09e8d457ab85f5361cdb83ec215
Dung24-6 <70428921+Dung24-6@users.noreply.github.com>
2021-09-29 11:08:25 +0700

    Word.java

---
+++ Word.java
@@ -0,0 +1,10 @@
+public class Word {
+    public static String word_target(String s) {
+        return " ";
+
+    }
+    public static String word_explain(String s) {
+        return " ";
+    }
+
+}

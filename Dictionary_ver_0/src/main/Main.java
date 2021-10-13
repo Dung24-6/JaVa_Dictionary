87f2ff354d80ba6b8c6d8a4e16c53c0776e74719
Minh Đức <80568469+minhduc-mll@users.noreply.github.com>
2021-10-13 17:25:07 +0700

    Commandline-ver-0

---
+++ Dictionary_ver_0/src/main/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DictionaryCommandline d = new DictionaryCommandline();
+        d.dictionaryBasic();
+    }
+}

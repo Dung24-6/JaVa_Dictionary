30c890642715b7b23b3ba530124cdeaed54d5ceb
Dung24-6 <70428921+Dung24-6@users.noreply.github.com>
2021-10-19 16:30:00 +0700

    Thêm package Voice

--- Dictionary/src/Voice/Speech.java
+++ Dictionary/src/Voice/Speech.java
@@ -5,7 +5,7 @@ import com.sun.speech.freetts.VoiceManager;
 public class Speech {
     private static Voice[] voices;
 
-    public  static void main ( String[]args) throws  Exception {
+    public  static void main ( String[]args)  {
 
         System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
 
@@ -17,6 +17,22 @@ public class Speech {
         voice.allocate();
         voice.speak("Hi,My name is Ha Van Quoc Dung");
         voice.deallocate();
+        //bản trên để test
+
+        //Hàm thêm vào on-click
+        /*
+        private void SpeechActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SpeechActionPerformed
+
+            System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+            vm = VoiceManager.getInstance();
+            Voice voice = VoiceManager.getInstance().getVoice("kevin16");
+            voice.allocate();
+            voice.speak(jTextField1.getText());
+            voice.deallocate();
+            }
+
+            // Gọi hàm
+         */
 
 
 
